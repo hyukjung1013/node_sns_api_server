f983d3429845f4a55988688c77b18972098fb1d3
hyukjung <hyukjung1013@gmail.com>
2019-06-06 15:30:16 +0900

    5th Commit: (1) ‘express-rate-limit’ added to limit usage. (2) This communicates with node_sns_api_client(3rd Commit).

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const v1router = require('./v1');
+const v2router = require('./v2');
 
 const router = express.Router();
 
 router.use('/v1', v1router);
+router.use('/v2', v2router);
 
 module.exports = router;
--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const RateLimit = require('express-rate-limit');
 
 exports.isLoggedIn = (req, res, next) => {
     if(req.isAuthenticated()) {
@@ -33,3 +34,22 @@ exports.verifyToken = (req, res, next) => {
         });
     }
 }; 
+
+exports.apiLimiter = new RateLimit({
+    windowMs: 60 * 1000,    // 1minute
+    max: 5,                 // 5 times a minute  
+    delayMs: 0,
+    handler(req, res) {
+        res.status(this.statusCode).json({
+            code: this.statusCode,
+            message: 'You can request 5 times a minute.'
+        });
+    }
+});
+
+exports.deprecated = (req, res) => {
+    res.status(410).json({
+        code: 410,
+        message: 'New version released. Use new version.'
+    });
+}
