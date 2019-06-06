a4d5071bb55d219bcfebdb749fdeef33b74b0cba
hyukjung <hyukjung1013@gmail.com>
2019-06-06 15:20:41 +0900

    4th Commit: (1) REST API endpoint added. (2) router.get('/posts/my', ...} (3) router.get('/search/:hashtag’, ...) (4) This communicates with a client 'node_sns_api_client(2nd Commit).

--- routes/api/v1.js
+++ routes/api/v1.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const { verifyToken } = require('../middlewares');
-const { Domain, User } = require('../../models');
+const { Domain, User, Post, Hashtag } = require('../../models');
 
 const router = express.Router();
 
@@ -29,7 +29,7 @@ router.post('/token', async (req, res) => {
             nickname: domain.user.nickname
         }, process.env.JWT_SECRET, {
             expiresIn: '1m',    // 1minute
-            issuer: 'yologrammer'
+            issuer: 'YOLO corp.'
         });
 
         return res.json({
@@ -49,6 +49,48 @@ router.post('/token', async (req, res) => {
 
 router.get('/test', verifyToken, (req, res) => {
     res.json(req.decoded);
+});
+
+router.get('/posts/my', verifyToken, async (req, res) => {
+    Post.findAll( { where: { userId: req.decoded.id } } )
+    .then((posts) => {
+        res.json({
+            code: 200,
+            payload: posts
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+        return res.status(500).json({
+            code: 500,
+            message: 'Server Error.'
+        });
+    });
+});
+
+router.get('/posts/hashtag/:title', verifyToken, async (req, res) => {
+    try {
+        const hashtag = await Hashtag.findOne({ where: {title: req.params.title} });
+        if (!hashtag) {
+            return res.status(404).json({
+                code: 404,
+                message: 'No search result.'
+            });
+        }
+
+        const posts = await hashtag.getPosts();
+        return res.json({
+            code: 200,
+            payload: posts
+        });
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            code: 500,
+            message: 'Server Error'
+        });
+    }
 });
 
 module.exports = router;
